8bca8c0f8d9a2d6c0aa83c787770cc991badca1c
Daniele Volpi <dvi@medicuja.com>
2020-12-10 09:38:06 +0100

    reformat day 09

--- day09.py
+++ day09.py
@@ -2,23 +2,23 @@ P = 25
 
 nums = []
 num = 0
-for n in open('data/day09.txt').read().splitlines():
+for n in open("data/day09.txt").read().splitlines():
     nums.append(int(n))
 
 for i in range(P, len(nums)):
-    preample = set(nums[i-P:i])
+    preamble = set(nums[i - P : i])
     num = nums[i]
     adds = []
-    for x in preample:
-        if (num - x) in preample:
-            adds.append(x) 
+    for x in preamble:
+        if (num - x) in preamble:
+            adds.append(x)
     if sum(adds) == 0:
-        print('P1:', num)
+        print("P1:", num)
         break
 
 finished = False
 for i in range(2, len(nums)):
-    cont = nums[i-2:i]
+    cont = nums[i - 2 : i]
     s = sum(cont)
     while s < num and not finished:
         cont.append(nums[i])
@@ -26,5 +26,5 @@ for i in range(2, len(nums)):
         s = sum(cont)
         if s == num:
             print(min(cont), max(cont))
-            print('P2:', min(cont) + max(cont))
+            print("P2:", min(cont) + max(cont))
             finished = True
